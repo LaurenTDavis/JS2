3ea532df67350c5efe205f35b6d24e0638ff201b
Student <student@RU-009.local>
2015-09-07 11:46:07 -0600

    alerts

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 var userInput = prompt("Enter a word")
+var inputLength = userInput.length;
+var thirdChar = userInput.charAt(2);
 
-alert("You entered: " + userInput + "\n" + "Length: " + userInput.length)
+alert("You entered: " + userInput + "\n" + "Length: " + inputLength + "\n" + "Third Character: " + thirdChar + "\n" + "Lowercase: " + userInput.toLowerCase() + "\n" + "Uppercase: " + userInput.toUpperCase() + "\n" + "Example: " + userInput + " is a word." + "\n" + "Subword: " + userInput.substring(1, 4));
